61d05b787abfc1995e8b84cd2052fdca62ceaab9
panfairy <leonye@canway.net>
2022-03-19 19:20:31 +0800

    init

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,125 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div>
+<!--    <div class="left">-->
+<!--      <img class="leftImg" src="@/assets/demo.jpeg" alt="" />-->
+<!--      <div v-show="topShow" class="top" :style="topStyle"></div>-->
+<!--      <div>-->
+<!--        <button @click="magnify(0, 0)">放大(0, 0)-(100, 100)两倍</button>-->
+<!--      </div>-->
+<!--    </div>-->
+<!--    <div v-show="rShow" class="right">-->
+<!--      <img-->
+<!--          :style="r_img"-->
+<!--          class="rightImg"-->
+<!--          src="@/assets/demo.jpeg"-->
+<!--          alt=""-->
+<!--      />-->
+<!--      <div v-show="topShow" class="top" :style="rightStyle"></div>-->
+<!--    </div>-->
+    <button @click="fn()">放大</button>
+    <magnify
+        :img-url="'@/assets/demo.jpeg'"
+        :width="'400px'"
+        :height="'400px'"
+        :box-style="boxStyle"
+        :show-type="rShow"
+        :zoom-rate="2"
+        :line-style="'2px solid red'"
+    ></magnify>
   </div>
 </template>
 
 <script>
+import magnify from "@/components/magnify";
+
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
-  }
-}
+  components: {
+    magnify
+  },
+  data() {
+    return {
+      topStyle: { transform: '' },
+      rightStyle: {},
+      r_img: {},
+      topShow: false,
+      rShow: false,
+      boxStyle: [
+        {
+          width: '100px',
+          height: '100px',
+          position: 'absolute',
+          top: '0',
+          left: '0',
+          border: '3px solid red'
+        },
+        {
+          width: '150px',
+          height: '150px',
+          position: 'absolute',
+          top: '100px',
+          left: '100px',
+          border: '2px solid red'
+        }
+      ]
+    }
+  },
+  methods: {
+    fn () {
+      this.rShow = !this.rShow
+    },
+    magnify (topX, topY) {
+      this.topStyle.transform = `translate(${topX}px, ${topY}px)`
+      this.rightStyle = `width: 400px; height: 400px;`
+      this.r_img.transform = `translate(-${2 * topX}px, -${2 * topY}px)`
+      this.rShow = !this.rShow
+      this.topShow = !this.topShow
+    }
+  },
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
+  .rightImg {
+    display: inline-block;
+    width: 800px;
+    height: 800px;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  .right {
+    margin-left: 412px;
+    /*width: 400px;*/
+    /*height: 400px;*/
+    width: 800px;
+    height: 800px;
+    border: 2px solid red;
+    position: relative;
+    overflow: hidden;
+  }
+
+  .top {
+    width: 200px;
+    height: 200px;
+    border: 1px solid red;
+    opacity: 0.4;
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
+
+  .leftImg {
+    width: 400px;
+    height: 400px;
+    display: inline-block;
+  }
+
+  .left {
+    width: 400px;
+    height: 400px;
+    border: 1px solid teal;
+    float: left;
+    position: relative;
+  }
 </style>
---
+++ src/components/magnify.vue
@@ -0,0 +1,75 @@
+<template>
+  <div>
+    <div class="origin" v-show="showType">
+      <img
+          src="@/assets/demo.jpeg"
+          :style="originImg"
+          alt=""
+      />
+      <div v-for="item in boxStyle" :style="item"></div>
+    </div>
+    <div class="large" v-show="!showType">
+      <img
+          src="@/assets/demo.jpeg"
+          :style="largeImg"
+          alt=""
+      />
+      <div v-for="item in boxStyle" :style="item"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    imgUrl: {
+      type: String
+    },
+    showType: {
+      type: Boolean,
+      default: true
+    },
+    zoomRate: {
+      type: Number,
+      default: 1
+    },
+    width: {
+      type: String,
+      default: '400px'
+    },
+    height: {
+      type: String,
+      default: '400px'
+    },
+    boxStyle: {
+      type: Array
+    },
+    lineStyle: {
+      type: String,
+      default: '1px solid red'
+    }
+  },
+  data() {
+    return {
+      originImg: {
+        width: this.width,
+        height: this.height,
+      },
+      largeImg: {
+        width: this.width,
+        height: this.height,
+      }
+    }
+  }
+}
+</script>
+
+<style scoped>
+.origin {
+
+}
+
+.large {
+
+}
+</style>
