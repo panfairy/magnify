4c369f0707aa38c58e5216ad1937962dec37b35f
panfairy <leonye@canway.net>
2022-03-20 11:11:40 +0800

    complete

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,49 +1,27 @@
 <template>
   <div>
-<!--    <div class="left">-->
-<!--      <img class="leftImg" src="@/assets/demo.jpeg" alt="" />-->
-<!--      <div v-show="topShow" class="top" :style="topStyle"></div>-->
-<!--      <div>-->
-<!--        <button @click="magnify(0, 0)">放大(0, 0)-(100, 100)两倍</button>-->
-<!--      </div>-->
-<!--    </div>-->
-<!--    <div v-show="rShow" class="right">-->
-<!--      <img-->
-<!--          :style="r_img"-->
-<!--          class="rightImg"-->
-<!--          src="@/assets/demo.jpeg"-->
-<!--          alt=""-->
-<!--      />-->
-<!--      <div v-show="topShow" class="top" :style="rightStyle"></div>-->
-<!--    </div>-->
     <button @click="fn()">放大</button>
     <magnify
         :width="400"
         :height="400"
-        :zoom="[0, 0]"
+        :zoom="[50, 50]"
         :zoom-rate="2"
+        :img="'demo.jpeg'"
         :box-style="boxStyle"
         :show-type="rShow"
     ></magnify>
-    <yuanxian style="position: relative; top:800px;"></yuanxian>
   </div>
 </template>
 
 <script>
-import magnify from "@/components/magnify";
-import yuanxian from "@/components/yuanxian";
+import magnify from "@/components/magnify"
 
 export default {
   components: {
-    magnify,
-    yuanxian
+    magnify
   },
   data() {
     return {
-      topStyle: { transform: '' },
-      rightStyle: {},
-      r_img: {},
-      topShow: false,
       rShow: true,
       boxStyle: [
         {
@@ -67,64 +45,11 @@ export default {
   },
   methods: {
     fn () {
-      // this.boxStyle.forEach(item => {
-      //   item.transform = `translate(${item.top}, ${item.left})`
-      // })
       this.rShow = !this.rShow
-    },
-    magnify (topX, topY) {
-      this.topStyle.transform = `translate(${topX}px, ${topY}px)`
-      this.rightStyle = `width: 400px; height: 400px;`
-      this.r_img.transform = `translate(-${2 * topX}px, -${2 * topY}px)`
-      this.rShow = !this.rShow
-      this.topShow = !this.topShow
     }
   },
 };
 </script>
 
 <style scoped>
-  .rightImg {
-    display: inline-block;
-    width: 800px;
-    height: 800px;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-
-  .right {
-    margin-left: 412px;
-    /*width: 400px;*/
-    /*height: 400px;*/
-    width: 800px;
-    height: 800px;
-    border: 2px solid red;
-    position: relative;
-    overflow: hidden;
-  }
-
-  .top {
-    width: 200px;
-    height: 200px;
-    border: 1px solid red;
-    opacity: 0.4;
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-
-  .leftImg {
-    width: 400px;
-    height: 400px;
-    display: inline-block;
-  }
-
-  .left {
-    width: 400px;
-    height: 400px;
-    border: 1px solid teal;
-    float: left;
-    position: relative;
-  }
 </style>
--- src/components/magnify.vue
+++ src/components/magnify.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="origin" :style="originImg" v-show="showType">
       <img
-          src="@/assets/demo.jpeg"
+          :src="getImg"
           :style="originImg"
           alt=""
       />
@@ -10,7 +10,7 @@
     </div>
     <div class="large" :style="originImg" v-show="!showType">
       <img
-          src="@/assets/demo.jpeg"
+          :src="getImg"
           :style="largeImg"
           alt=""
       />
@@ -26,6 +26,9 @@ export default {
       type: Boolean,
       default: true
     },
+    img: {
+      type: String
+    },
     zoomRate: {
       type: Number,
       default: 1
@@ -35,17 +38,20 @@ export default {
       default: [0, 0]
     },
     width: {
-      type: Number
+      type: Number || String
     },
     height: {
-      type: Number
+      type: Number || String
     },
     boxStyle: {
-      type: Array
+      type: Array,
+      default: []
     }
   },
   data() {
     return {
+      // public img: string = require(this.img)
+      getImg: require('@/assets/' + this.img),
       originImg: {
         width: this.width + 'px',
         height: this.height + 'px',
@@ -53,7 +59,7 @@ export default {
       largeImg: {
         width: this.width * this.zoomRate + 'px',
         height: this.height * this.zoomRate + 'px',
-        transform: `translate(-${this.zoom[0]}px, -${this.zoom[1]}px)`
+        transform: `translate(-${this.zoom[0] * this.zoomRate}px, -${this.zoom[1] * this.zoomRate}px)`
       },
       box: []
     }
@@ -61,16 +67,54 @@ export default {
   mounted() {
     this.boxHandle()
   },
+  // computed: {
+  //   getImg() {
+  //     return {
+  //       ...this.img,
+  //       img: this.img && require(`../assets/${this.img}`)
+  //     }
+  //   }
+  // },
   methods: {
+    // const unit = {
+    //   px: 1,
+    //   vm: 1 << 1,
+    //   vh: 1 << 2,
+    //   rem: 1 << 3
+    // }
+    // getImg() {
+    //   const url = '@/assets/demo.jpeg'
+    //   return require(url)
+    //   const imgUrl = 'demo.jpeg';
+    //   let img = require('../assets/'+imgUrl);
+    // },
     boxHandle() {
+      if (this.boxStyle.length === 0) return
       this.box = JSON.parse(JSON.stringify(this.boxStyle))
       this.box.forEach(item => {
         item.width = parseInt(item.width) * this.zoomRate + 'px'
         item.height = parseInt(item.height) * this.zoomRate + 'px'
-        item.top = parseInt(item.top) * this.zoomRate + 'px'
-        item.left = parseInt(item.left) * this.zoomRate + 'px'
+        item.top = (parseInt(item.top) - this.zoom[1]) * this.zoomRate + 'px'
+        item.left = (parseInt(item.left) - this.zoom[0]) * this.zoomRate + 'px'
       })
     }
+    // isObject(x) {
+    //   return Object.prototype.toString.call(x) === '[object Object]'
+    // },
+    // clone(source) {
+    //   if (!isObject(source)) return source
+    //   let target = {}
+    //   for(let i in source) {
+    //     if (source.hasOwnProperty(i)) {
+    //       if (typeof source[i] === 'object') {
+    //         target[i] = clone(source[i])
+    //       } else {
+    //         target[i] = source[i]
+    //       }
+    //     }
+    //   }
+    //   return target
+    // }
   }
 }
 </script>
